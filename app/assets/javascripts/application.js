7f24fed9272fc9256ca1d06788956f58df779f60
diegotf30 <diegotf30@gmail.com>
2018-08-26 05:31:18 -0500

    Fixed facebook SDK include

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,19 +16,10 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-window.fbAsyncInit = function() {
-FB.init({
-  appId            : '1893011574338670',
-  autoLogAppEvents : true,
-  xfbml            : true,
-  version          : 'v3.1'
-});
-};
-
-(function(d, s, id){
- var js, fjs = d.getElementsByTagName(s)[0];
- if (d.getElementById(id)) {return;}
- js = d.createElement(s); js.id = id;
- js.src = "https://connect.facebook.net/en_US/sdk.js";
- fjs.parentNode.insertBefore(js, fjs);
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
